6c6bc548ecc9b33c7dc191cdc4161e607217035a
shnymywk <shnymywk@gmail.com>
2026-05-23 04:37:40 +0900

    ⚗️ reducksパターンの理解を深めるためのテストコーディング

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -15,17 +15,21 @@
 //     )
 // }
 
-import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom'; // 1. インポート
+import { signInAction } from '../reducks/users/actions'
 
 const Login = () => {
+    const dispatch = useDispatch();
     const navigate = useNavigate(); // 2. フックを呼び出し
 
     return (
         <div>
             <h2>ログイン</h2>
             {/* 3. dispatchを使わず、直接 navigate を呼び出す */}
-            <button onClick={() => navigate('/')}>
+            <button onClick={
+                dispatch(signInAction({uid:"0001", username:"torahack"})),
+                () => navigate('/')}>
                 ログインする
             </button>
         </div>
